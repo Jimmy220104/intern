f7432c95b6b98fcfe5df9188f9b004a2d848b255
Jimmy220104 <jimmymahajan725@gmail.com>
2026-01-17 11:31:43 +0530

    Refactor date conversion logic and update output comments for clarity

--- Task6(Dates).js
+++ Task6(Dates).js
@@ -33,7 +33,6 @@
 
 // 4 input Today: 12 June 2025
 //   output 1 : 7 days from now: 19 June 2025
-//   output 2 : 30 days ago: 13 May 2025
 
 // let input = new Date("June 12, 2025")
 // let month = ["January", "february", "March", "April", "May", "June", "July", 
@@ -45,7 +44,8 @@
 //  let year = input.getFullYear();
 
 //  console.log(`7 days after: ${date1} ${month1} ${year}`);
-
+// ------------------------------------------------------------------------------------------------------------------------------
+//   output 2 : 30 days ago: 13 May 2025
 
 // let input = new Date("June 12, 2025")
 // let month = ["January", "february", "March", "April", "May", "June", "July", 
@@ -62,28 +62,34 @@
 
 // 5 Convert UTC Time to IST and AUS
 //   input = "2025-06-12T10:30:25Z"; 
+//   output 1 : "12 June, 2025, 4:00 PM" (IST)
 //   output 2 : "12 June, 2025, 8:00 PM" (AUS)
 
-let date = "2025-06-12T10:30:25Z"; 
-let month = ["January", "february", "March", "April", "May", "June", "July", 
-            "August", "September", "October", "November", "December"];
+    let date = "2025-06-12T10:30:25Z"; 
+    let month = ["January", "february", "March", "April", "May", "June", "July", 
+                "August", "September", "October", "November", "December"];
 
-let date1 = new Date(date);
+    let date1 = new Date(date);
 
 function time(date1){
-let day = date1.getDate();
-let month1 = month[date1.getMonth()];
-let year = date1.getFullYear();
-let hour = date1.getHours();
-// let finHour = date1.getHours();
-let minutes = date1.getMinutes();
-let amPm = hour >= 12 ? "PM" : "AM";
-return `${day} ${month1} ${year} ${hour}:${minutes} ${amPm}`;
-}
-let aus = new Date(date1);
-aus.setHours(aus.getHours() + 5);
-aus.setMinutes(aus.getMinutes() + 30);
+    let day = date1.getDate();
+    let month1 = month[date1.getMonth()];
+    let year = date1.getFullYear();
+    let hour = date1.getHours();
+    let minutes = date1.getMinutes();
+    let amPm = hour >= 12 ? "PM" : "AM";
 
+    return `${day} ${month1} ${year} ${hour}:${minutes} ${amPm}`;
+}
+// output: 1
+    let ind = new Date(date1);
+    
+// output: 2
+    let aus = new Date(date1);
+    aus.setHours(aus.getHours() + 4);
+    aus.setMinutes(aus.getMinutes() + 30);
+
+console.log(time(ind));
 console.log(time(aus));
 
 
